6e3b1a12ef50de63aadf0fed579ea88dc428ba2d
Ritsuki Goto <develop@mizucoffee.net>
2018-05-24 02:15:45 +0900

    [refactor] scss

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,31 +1,31 @@
 module.exports = () => {
   let button = Array.from(document.getElementsByClassName('subdomain'))
-  let label = document.getElementById('label')
   let welcome = document.getElementById('welcome')
   let headbox = document.getElementById('head-box')
-  let listbox = document.getElementById('list-box')
-  let listdomain = document.getElementById('list-domain')
 
   button.forEach(e => {
     e.addEventListener('click',() => {
       e.classList.add('title')
-      listbox.classList.add('to-head')
+      document.getElementById('list-box').classList.add('to-head')
 
       button.filter(i => i != e).forEach(i => i.classList.add('fade-out'))
-      label.classList.add('fade-out')
-
+      document.getElementById('label').classList.add('fade-out')
       headbox.classList.add('fade-out')
-      Array.from(headbox.children).forEach(i => i.classList.add('hide'))
+
+
       setTimeout(() => {
-        welcome.classList.add('inv')
+        welcome.classList.add('inv-text')
+        Array.from(headbox.children).forEach(i => i.classList.add('hide'))
       },1000)
+
       setTimeout(() => {
-        button.filter(i => i != e).forEach(i => i.classList.add('none'))
         e.classList.add('left')
-        listdomain.classList.add('show')
-        welcome.classList.remove('inv')
+        button.filter(i => i != e).forEach(i => i.classList.add('none'))
+        document.getElementById('list-domain').classList.add('show')
+
         welcome.innerText = "About"
-      } ,2500)
+        welcome.classList.remove('inv-text')
+      },2000)
     })
   })
 
