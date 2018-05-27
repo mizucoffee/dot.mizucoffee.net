47706bed65e5afbeb45dad289fd70a90796a8a6b
Ritsuki Goto <develop@mizucoffee.net>
2018-05-28 01:42:03 +0900

    [add] support responsive

--- gulpfile.js
+++ gulpfile.js
@@ -42,13 +42,13 @@ gulp.task('res',() => {
 gulp.task('default', ['clean','pug','js','scss','res'], () => {})
 
 gulp.task('watch', ['default'], () => {
-  browserSync({
-    server: {baseDir: 'build'},
-    port: 3000
-  })
+// browserSync({
+//    server: {baseDir: 'build'},
+//    port: 3000
+//  })
 
-  gulp.watch('./build/**/*',() => browserSync.reload())
-  gulp.watch('./build/*',() => browserSync.reload())
+//  gulp.watch('./build/**/*',() => browserSync.reload())
+//  gulp.watch('./build/*',() => browserSync.reload())
   gulp.watch('./src/scss/*',['scss'])
   gulp.watch(['./src/pug/*','./data.json'] ,['pug'])
   gulp.watch('./src/js/*'  ,['js'])
